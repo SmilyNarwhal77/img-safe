aeadd25460366ef6c094a2fff46cae22f83f8c25
Keegan <keegan23.tan@gmail.com>
2025-06-05 10:53:55 +0800

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,99 @@
+//=====INIT=======
+let personalsequence = "";
+let attemptsequence = "";
+let state = 0;
+const unlockbutton = document.getElementById("unlock");
+const lockbutton = document.getElementById("lock");
+const clearbutton = document.getElementById("clear");
+const imginput = document.getElementById("imgInput");
+const imgdisplay = document.getElementById("img");//check
+function changestate() {
+    state = 1;
+}
+function lock() {
+  if(state == 1) {
+    state = 0;
+  } else if(!imginput.value) {
+    //check
+    
+function completepword() {
+  
+function check() {
+    if(personalsequence === attemptsequence && personalsequence.length != 0) {
+      unlock();
+    } else if(personalsequence.length === attemptsequence.length) {
+      failed();
+    } else {
+      return;
+    }
+}
+function b0() {
+    if(state == 1) {
+      personalsequence += "0";
+    } else {
+      attemptsequence += "0";
+      check(); 
+}
+function b1() {
+    if(state == 1) {
+      personalsequence += "1";
+    } else {
+      attemptsequence += "1";
+      check(); 
+}
+function b2() {
+    if(state == 1) {
+      personalsequence += "2";
+    } else {
+      attemptsequence += "2";
+      check(); 
+}
+function b3() {
+    if(state == 1) {
+      personalsequence += "3";
+    } else {
+      attemptsequence += "3";
+      check(); 
+}
+function b4() {
+    if(state == 1) {
+      personalsequence += "4";
+    } else {
+      attemptsequence += "4";
+      check(); 
+}
+function b5() {
+    if(state == 1) {
+      personalsequence += "5";
+    } else {
+      attemptsequence += "5";
+      check(); 
+}
+function b6() {
+    if(state == 1) {
+      personalsequence += "6";
+    } else {
+      attemptsequence += "6";
+      check(); 
+}
+function b7() {
+    if(state == 1) {
+      personalsequence += "7";
+    } else {
+      attemptsequence += "7";
+      check(); 
+}
+function b8() {
+    if(state == 1) {
+      personalsequence += "8";
+    } else {
+      attemptsequence += "8";
+      check(); 
+}
+function b9() {
+    if(state == 1) {
+      personalsequence += "9";
+    } else {
+      attemptsequence += "9";
+      check(); 
+}
